bb21fcbb06a0f9da4abec85f59669d6385ddbcc2
Rishabh Chawla <rishabh31121999@gmail.com>
2020-07-24 16:55:21 +0530

    feat: final project

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
-#include <GL/glut.h>
+#include <GL/freeglut.h>
 
 #define INF_MASS 10000
-#define SCR_SIZE 550
-#define G 0.03
+#define SCR_SIZE 700
+#define G 0.005
 
 using namespace std;
 
@@ -17,6 +17,7 @@ void display();
 void mouse(int, int, int, int);
 void mouseMotion(int, int);
 void removeUniverse();
+void RenderString(float x, float y, void *font, string s);
 void keyboard(unsigned char, int, int);
 
 int Mx, My, WIN;
@@ -32,7 +33,7 @@ vector<Body*> universe; // Contains all Suns and Planets
  * @note
  * This includes all basic operations between
  * two vectors that are required by the program
- * **/ 
+ * **/
 class Vector {
 
     float x, y;
@@ -67,16 +68,17 @@ protected:
     Vector v;           // Velocity Components
     float radius;       // Radius of body
     float mass;         // Mass of the body
-    float* color;     // Color channels of the body
+    float color[3];     // Color channels of the body
 
 public:
-    Body(Vector pos, Vector v, float radius, float mass, float* color);
+    Body(Vector pos, Vector v, float radius, float mass, float color[3]);
     Vector* position();
     Vector* velocity();
-    void draw();
+    void draw(int i);
     friend Vector field(Body* a, Body* b);    // Update Velocity of *this due to field of body
     float getMass();
     float getRadius();
+    virtual char type() = 0;
 };
 
 /**
@@ -89,8 +91,9 @@ public:
  * **/
 class Planet: public Body {
 public:
-    Planet(Vector pos, Vector v, float* color, float radius = 1, float mass = 100)
+    Planet(Vector pos, Vector v, float color[3], float radius = 5, float mass = 1000)
     :Body(pos, v, radius, mass, color){};
+    char type();
 };
 
 /**
@@ -103,8 +106,9 @@ public:
  * **/
 class Sun: public Body {
 public:
-    Sun(Vector pos, Vector v, float* color, float radius = 10, float mass = INF_MASS)
+    Sun(Vector pos, Vector v, float color[3], float radius = 20, float mass = INF_MASS)
     :Body(pos, v, radius, mass, color){};
+    char type();
 };
 
 Vector::Vector() {
@@ -114,14 +118,14 @@ Vector::Vector(float a, float b) {
     x = a; y = b;
 }
 void Vector::setX(float x) {
-    this -> x = x;
+    this->x = x;
 }
 void Vector::setY(float y) {
-    this -> y = y;
+    this->y = y;
 }
 
 pair<float, float> Vector::cords() {
-    return {x, y};
+    return pair<float, float>(x, y);
 }
 
 Vector operator + (Vector a, Vector b) {
@@ -135,7 +139,7 @@ Vector operator * (Vector a, float f) {
 }
 
 float distance (Vector a, Vector b){
-    float c = (a.x-b.x)*(a.x-b.x) + (a.y - b.y)*(a.y - b.y);
+    float c = (a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y);
     return(sqrt(c));
 }
 
@@ -144,34 +148,38 @@ Body::Body(Vector pos, Vector v, float radius, float mass, float* color) {
     this->v = v;
     this->mass = mass;
     this->radius = radius;
-    this->color = color;
+    for (int i = 0; i < 3; i++) {
+        this->color[i] = color[i];
+    }
 }
 
 Vector * Body::position(){
    return & pos;
 }
+
 Vector * Body::velocity(){
     return &v;
 }
 
-
-void Body::draw(){
+void Body::draw(int i){
     glColor3f(color[0], color[1], color[2]);
     glBegin(GL_POLYGON);
-    for(float a = 0; a < 2*M_PI; a+=0.2)
-            glVertex2f(radius*cos(a) + pos.cords().first, radius*sin(a) + pos.cords().second);
+    for(float a = 0; a < 2*M_PI; a+=0.2) {
+        glVertex2f(radius*cos(a) + position() -> cords().first, radius*sin(a) + position() -> cords().second);
+        // cout << radius*cos(a) + pos.cords().first << " " << radius*sin(a) + pos.cords().second << "\n";
+    }
     glEnd();
 }
 
 Vector field(Body* a, Body* b) {
     Vector E;
-    float d = distance(a -> pos, b -> pos);
-    float ax = a -> pos.cords().first;
-    float ay = a -> pos.cords().second;
-    float bx = b -> pos.cords().first;
-    float by = b -> pos.cords().second;
-    E.setX((G*(b -> mass)/(d*d))*(bx - ax)/d);
-    E.setY((G*(b -> mass)/(d*d))*(by - ay)/d);
+    float d = distance(*(a -> position()), *(b -> position()));
+    float ax = a -> position() -> cords().first;
+    float ay = a -> position() -> cords().second;
+    float bx = b -> position() -> cords().first;
+    float by = b -> position() -> cords().second;
+    E.setX(G*(b -> mass)/(d*d) * (bx - ax)/d);
+    E.setY(G*(b -> mass)/(d*d) * (by - ay)/d);
     return E;
 }
 
@@ -182,12 +190,35 @@ float Body::getRadius() {
     return radius;
 }
 
+char Planet::type() {
+    return 'p';
+}
+
+char Sun::type() {
+    return 's';
+}
+
 void display()
 {
+    int mass = 0;
+    int Suns = 0, Planets = 0;
+    for (auto b : universe){
+        mass += b -> getMass();
+        if (b -> type() == 's') Suns++;
+        else Planets++;
+    }
+
         glClear(GL_COLOR_BUFFER_BIT);
+        RenderString(20 - SCR_SIZE/2, 50 - SCR_SIZE/2, GLUT_BITMAP_HELVETICA_18, "Planet Simulation");
+        RenderString(-200 + SCR_SIZE/2, 50 - SCR_SIZE/2, GLUT_BITMAP_HELVETICA_12, "Statistics ");
+        RenderString(-200 + SCR_SIZE/2, 65 - SCR_SIZE/2, GLUT_BITMAP_HELVETICA_10, "Bodies in Universe: " + to_string(universe.size()));
+        RenderString(-200 + SCR_SIZE/2, 80 - SCR_SIZE/2, GLUT_BITMAP_HELVETICA_10, "Total Mass: " + to_string(mass));
+        RenderString(-200 + SCR_SIZE/2, 95 - SCR_SIZE/2, GLUT_BITMAP_HELVETICA_10, "Number of Suns: " + to_string(Suns));
+        RenderString(-200 + SCR_SIZE/2, 110 - SCR_SIZE/2, GLUT_BITMAP_HELVETICA_10, "Number of Planets: " + to_string(Planets));
+
 
         for(int i = 0; i < universe.size(); i++)
-            universe[i] -> draw();
+            universe[i] -> draw(i+1);
  
         glFlush();
         glutSwapBuffers();
@@ -198,12 +229,15 @@ void timer(int)
     display();
     if(PRESSED_LEFT)
     {
-        float color[3] = {1, 1, 0};
+        float color[3];
+        color[0] = rand()%200 / 200.0;
+        color[1] = rand()%200 / 200.0;
+        color[2] = rand()%200 / 200.0;
 
-        Vector pos((float)Mx, (float)My), v((float)0, (float)0);
+        Vector pos((float)Mx, (float)My), v((float)0.5, (float)0);
         Planet* p = new Planet(pos, v, color);
         universe.push_back(p);
-        cout << "P "<< universe.size() << " ";
+        // cout << "P "<< universe.size() << " ";
         PRESSED_LEFT = false;
     }
 
@@ -214,7 +248,7 @@ void timer(int)
         Vector pos((float)Mx, (float)My), v((float)0, (float)0);
         Sun* s = new Sun(pos, v, color);
         universe.push_back(s);
-        cout << "S " << universe.size() << " ";
+        // cout << "S " << universe.size() << " ";
 
         PRESSED_RIGHT = false;
     }
@@ -232,17 +266,16 @@ void timer(int)
                 continue;   
 
             Body* p = universe[j];
-            float d = distance(b, p);
-
-            if(d > p -> getRadius())
+            float d = distance(*(b -> position()), *(p -> position()));
+            if(d > p -> getRadius() || d > SCR_SIZE)
             {
                 pair<float, float> E = field(b, p).cords();
                 pair<float, float> v = b -> velocity() -> cords();
                 b -> velocity() -> setX(v.first + E.first);
                 b -> velocity() -> setY(v.second + E.second);
             }
-            else
-                not_fall = false;
+            else not_fall = false;
+                // cout << "\n" << d << " " << p -> getRadius()
         }
 
         if(not_fall)   
@@ -250,10 +283,12 @@ void timer(int)
             pair<float, float> v = b -> velocity() -> cords();
             pair<float, float> p = b -> position() -> cords();
             b -> position() -> setX(p.first + v.first);
-            b -> velocity() -> setY(p.second + v.second);
+            b -> position() -> setY(p.second + v.second);
         }
-        else
+        else {
+            free(universe[i]);
             universe.erase(universe.begin()+i);
+        }
     }
 
     glutTimerFunc(1, timer, 0);
@@ -262,8 +297,8 @@ void timer(int)
 void mouse(int button, int state, int x, int y)
 {
         //set the coordinates
-        Mx = x;
-        My = y;
+        Mx = x - (SCR_SIZE/2);
+        My = y - (SCR_SIZE/2);
         // cout << Mx << " " << My << "\n";
         //check which button is pressed
         if(button == GLUT_LEFT_BUTTON)
@@ -282,6 +317,7 @@ void mouseMotion(int x, int y)
 
 void removeUniverse()
 {
+    for(auto b: universe) free(b);
     universe.erase(universe.begin(), universe.end());
 }
 
@@ -298,6 +334,15 @@ void keyboard(unsigned char key, int x, int y)
                 removeUniverse();
                 break;
     }
+}
+
+void RenderString(float x, float y, void *font, string s)
+{  
+  char *c;
+
+  glColor3f(1, 1, 1); 
+  glRasterPos2f(x, y);
+  glutBitmapString(font, (const unsigned char *)s.c_str());
 }
 
 int main(int argc, char **argv)
@@ -323,6 +368,6 @@ int main(int argc, char **argv)
     glutKeyboardFunc(keyboard);
     timer();
     glutMainLoop();
-
+    removeUniverse();
     return 0;
 }
